8f48ed8e241349e95434236bfe7805212c4c92fb
ansoncht <56498302+ansoncht@users.noreply.github.com>
2021-02-22 00:16:31 -0800

    display more result

--- search.py
+++ search.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas_datareader as pdr
 from datetime import datetime
 
-
 class Symbol:
     def __init__(self, symbol):
         self.symbol = symbol
@@ -13,6 +12,8 @@ class Symbol:
         data = pdr.get_data_stooq(symbols=self.symbol, start=datetime(2000, 1, 1), end=datetime.now())
         # print(data)
         ans = self.calculate(data)
+        # get competitor (market share +=
+        # show graph
         print('standard deviation for ' + self.symbol + ' is: ', round(ans, 5))
         # new_table = amd_data.assign(Return=amd_data.get('Adj Close') - amd_data.get('Open'))
         # print(new_table)
@@ -27,6 +28,7 @@ class Symbol:
 
     def calculate(self, data):
         new_table = data.assign(Return=data.get('Close') - data.get('Open'))
+        print (new_table)
         r_bar = new_table.get('Return').sum() / new_table.shape[0]
         top = 0
         for i in range(len(new_table)):
